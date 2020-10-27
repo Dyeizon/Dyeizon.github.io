a7a9f7a39871d559d91a9fadefcf988f1f0c10c9
Dyeizon <dyeizonque@gmail.com>
2020-10-26 23:34:14 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,33 @@ console.log("Script funcionando!");
 
 var isStarted = false;
 
+function move(direcao, px) {
+    if (direcao == "UP") {
+        var atual = (window.getComputedStyle(document.querySelector(".square")).top).replace("px", "   ");
+        atual = parseInt(atual, 10) - px;
+        document.querySelector(".square").style.top = atual.toString() + "px";
+    }
+
+    if (direcao == "LEFT") {
+        var atual = (window.getComputedStyle(document.querySelector(".square")).left).replace("px", "   ");
+        atual = parseInt(atual, 10) - px;
+        document.querySelector(".square").style.left = atual.toString() + "px";
+    }
+
+    if (direcao == "DOWN") {
+        var atual = (window.getComputedStyle(document.querySelector(".square")).top).replace("px", "   ");
+        atual = parseInt(atual, 10) + px;
+        document.querySelector(".square").style.top = atual.toString() + "px";
+    }
+
+    if (direcao == "RIGHT") {
+        var atual = (window.getComputedStyle(document.querySelector(".square")).left).replace("px", "   ");
+        atual = parseInt(atual, 10) + px;
+        document.querySelector(".square").style.left = atual.toString() + "px";
+    }
+}
+
+
 function verificador() {
     if (isStarted == false) {
         document.querySelector(".game").style.transition = "200ms";
@@ -16,6 +43,7 @@ function game(event) {
     if (true) {
         switch(event.keyCode) {
             case 119:
+                move("UP", 20);
                 break;
             case 97:
                 var atual = (window.getComputedStyle(document.querySelector(".square")).left).replace("px", "   ");
